527b45c997ac51577478d0b0dfed8639af6e8250
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-05 14:16:48 -0400

    Updated from Brython Server: 10/5/2015 2:16:49 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 import sys
-string = input("Please enter a string of text (the bigger the better): "
+string = input("Please enter a string of text (the bigger the better): ")
 b = list(string)
 b.reverse()
 for x in b:
