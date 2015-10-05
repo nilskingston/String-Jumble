f62959dd6bb183cf8d6f17bdf141c08f6208e9d7
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-05 14:50:11 -0400

    Updated from Brython Server: 10/5/2015 2:50:12 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,7 +27,7 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 
 string = input("Please enter a string of text (the bigger the better): ")
-print("You entered " + string + ". Now jumble it: ")
+print('You entered "'+string+'" . Now jumble it: ")
 b = list(string)
 b.reverse()
 for x in b:
